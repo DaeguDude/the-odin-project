e943c13fa86d358f502055358e72698df008f78e
DaeguDude <k3hppk@gmail.com>
2020-01-28 16:42:52 +0900

    Done Setting up, Worked on javascript fundamentals as well

---
+++ javascript-basics/test.js
@@ -0,0 +1,33 @@
+// // NaN - Not a Number 
+// var x = 100 / "Apple";
+// console.log(isNaN(x));
+
+// var x = 100 / "10";
+// console.log(isNaN(x));
+
+// var x = NaN;
+// var y = 5;
+// var z = x + y;
+// console.log(isNaN(z));
+
+// console.log(typeof NaN);
+
+// Infinity
+// var myNumber = 2;
+// var count = 0;
+// while (myNumber != Infinity) { // Execute until Infinity
+//   myNumber = myNumber * myNumber;
+//   count = count + 1;
+//   console.log(`Number that's been multiplied: ${count}`);
+//   console.log(myNumber);
+// }
+
+// if preceded by 0x, considered as hexadecimal
+var x = 123;
+var y = new Number(123);
+
+var x = new Number(500);
+var y = new Number(500);
+console.log(x == y);
+console.log(x === y);
+

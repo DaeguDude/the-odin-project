6e6ac30809a509169bada6e52af4d71ed54f7566
SangHak <k3hppk@gmail.com>
2020-03-30 18:15:09 +0900

    Going through javascript objects

---
+++ javascript/test.js
@@ -0,0 +1,14 @@
+const myObject = {
+  
+  property: 'Value!',
+  otherProperty: 77,
+  "obnoxious property": function() {
+
+  },
+  
+
+}
+
+console.log(myObject.variable);
+console.log(myObject.property);
+console.log(myObject["obnoxious property"]);
